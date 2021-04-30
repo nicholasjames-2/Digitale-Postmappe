567fd0700828f3beb4e376d52c7c5fed58c6d204
nicholasjames-2 <45181499+nicholasjames-2@users.noreply.github.com>
2021-04-30 20:15:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="de" class="no-js">
 	<head>
-		<!-- Bootstrap Intigration 
+		<!-- Bootstrap Intigration -->
 			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 		<!-- Bootstrap Intigration END -->
 		<!-- META -->
@@ -17,13 +17,39 @@
 		<!-- Sonstiges Wichtiges END -->
 	</head>
 	<body>
-		<div id="test">
-			<h1>Test123</h1>
-		</div>
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#">Fixed navbar</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					  <div class="collapse navbar-collapse" id="navbarCollapse">
+						<ul class="navbar-nav me-auto mb-2 mb-md-0">
+						   <li class="nav-item">
+							  <a class="nav-link active" aria-current="page" href="#">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+							</li>
+						</ul>
+						<form class="d-flex">
+							<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+							<button class="btn btn-outline-success" type="submit">Search</button>
+						</form>
+					</div>
+				</div>
+			</nav>
 		<footer>
 			<div class="footer">
 				<span>Entwickelt von <a href="https://nicholas-bennett.de/">Nicholas Bennett</a> | &copy; 2021</span>
 			</div>
 		</footer>
+
+		<!-- Skripts -->
+		<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+		<!-- Skripts END -->
 	</body>
 </html>
