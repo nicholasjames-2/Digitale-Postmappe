e5b727646488d3e504a87ce30dfd2d1ec49d013f
Nicholas Bennett <nicholas.bennett@gmx.de>
2021-04-30 19:46:26 +0200

    first push

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="de" class="no-js">
+	<head>
+		<!-- Bootstrap Intigration 
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+		<!-- Bootstrap Intigration END -->
+		<!-- META -->
+			<meta charset="utf-8">
+			<meta name="author" content="Nicholas Bennett | nicholas-bennett.de"
+			<meta name="description" content="Analoge Postmappen in Firmen digital Verwalten">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<!-- META END -->
+		<!-- Sonstiges Wichtiges -->
+			<title>Digitale Postmappe</title>
+			<link rel="stylesheet" href="/css/front-end.css">
+		<!-- Sonstiges Wichtiges END -->
+	</head>
+	<body>
+		<footer>
+			<div class="footer">
+				<span>Entwickelt von <a href="https://nicholas-bennett.de/">Nicholas Bennett</a> | &copy; 2021</span>
+			</div>
+		</footer>
+	</body>
+</html>
