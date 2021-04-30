c7dac248f23a02817a9bba5998823a8f11311c5d
Nicholas Bennett <nicholas.bennett@gmx.de>
2021-04-30 21:12:28 +0200

    Nav fix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<!-- Bootstrap Intigration END -->
 		<!-- META -->
 			<meta charset="utf-8">
-			<meta name="author" content="Nicholas Bennett | nicholas-bennett.de"
+			<meta name="author" content="Nicholas Bennett | nicholas-bennett.de">
 			<meta name="description" content="Analoge Postmappen in Firmen digital Verwalten">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			<meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -17,31 +17,32 @@
 		<!-- Sonstiges Wichtiges END -->
 	</head>
 	<body>
-			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-				<div class="container-fluid">
-					<a class="navbar-brand" href="#">Fixed navbar</a>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					  <div class="collapse navbar-collapse" id="navbarCollapse">
-						<ul class="navbar-nav me-auto mb-2 mb-md-0">
-						   <li class="nav-item">
-							  <a class="nav-link active" aria-current="page" href="#">Home</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">Link</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-							</li>
-						</ul>
-						<form class="d-flex">
-							<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-							<button class="btn btn-outline-success" type="submit">Search</button>
-						</form>
-					</div>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">Fixed navbar</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				  <div class="collapse navbar-collapse" id="navbarCollapse">
+					<ul class="navbar-nav me-auto mb-2 mb-md-0">
+					   <li class="nav-item">
+						  <a class="nav-link active" aria-current="page" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						</li>
+					</ul>
+					<form class="d-flex">
+						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success" type="submit">Search</button>
+					</form>
 				</div>
-			</nav>
+			</div>
+		</nav>
+		<br><br><br>
 		<footer>
 			<div class="footer">
 				<span>Entwickelt von <a href="https://nicholas-bennett.de/">Nicholas Bennett</a> | &copy; 2021</span>
